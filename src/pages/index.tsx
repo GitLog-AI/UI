ee22bab7f720a67954da19f4e4bec383cc28518a
srilokhkaruturi <srilokh.karuturi@gmail.com>
2023-04-20 17:00:15 -0500

    Make index server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -176,3 +176,12 @@ export default function Home() {
 }
 
 
+
+export async function getServerSideProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
+
+
