30f03194eece548cff5de35fbab950ae76eb54e2
srilokhkaruturi <srilokh.karuturi@gmail.com>
2023-04-24 12:36:52 -0500

    Add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useState } from 'react';
 import { LoginContext } from '@/context/LoginPopup';
 import { GeistProvider } from '@geist-ui/core';
+import ReactGA from "react-ga4";
+
 const lightTheme = createTheme({
   type: "light",
 });
@@ -15,6 +17,8 @@ const darkTheme = createTheme({
 
 
 export default function App({ Component, pageProps }: AppProps) {
+  ReactGA.initialize("G-M415S50XJS");
+
   const [showLogin, setShowLogin] = useState(false);
   return (
     // 2. Use at the root of your app
